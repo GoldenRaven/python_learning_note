5d60bb0d32d2b612538222d0ef081edf2d5cacf1
ligy <ligy-_-@outlook.com>
2020-02-14 20:38:12 +0800

    feat: add bin, oct, hex

--- nowcoder/huawei/5.py
+++ nowcoder/huawei/5.py
@@ -2,5 +2,11 @@ while True:
     try:
         s = input()
         print(int(s, 16))
+        # int(s, 2)
+        # int(s, 8)
+        # int(s, 16)
+        # bin(s)
+        # oct(s)
+        # hex(s)
     except:
         break
