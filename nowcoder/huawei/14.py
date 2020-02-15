5ef2669ad1eedf7e8b4023c0c4e449b7f1d5a85d
GoldenRaven <ligy-_-@outlook.com>
2020-02-15 17:14:41 +0800

    refactor: sorted(list) as dictionary

--- nowcoder/huawei/14.py
+++ nowcoder/huawei/14.py
@@ -3,8 +3,8 @@ ss = []
 for i in range(n):
     s = input()
     ss.append(s)
-ss.sort()
-# print()
-for i in range(n):
-    print(ss[i])
-    # print(ss[i], end='\n')
+# ss.sort()
+sss = sorted(ss)
+print()
+for i in sss:
+    print(i)
