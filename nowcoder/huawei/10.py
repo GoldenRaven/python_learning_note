0f081d3ac7bade8403cde90538284c2ddf12dff5
GoldenRaven <ligy-_-@outlook.com>
2020-02-15 10:25:58 +0800

    feat: change 10  to function

--- nowcoder/huawei/10.py
+++ nowcoder/huawei/10.py
@@ -1,6 +1,8 @@
 s = input()
 # s =  '1`a890-)(*&^%$#@#$%^&*()_P:"}||"?><./'\['
-l = list(s)
-ls = list(set(l))
-ls_filter = [x for x in ls if ( ord(x) > 0 & ord(x) < 128)]
-print(len(ls_filter))
+def count_ACSII(strings=''):
+    l = list(strings)
+    ls = list(set(l))
+    ls_filter = [x for x in ls if ( ord(x) > 0 & ord(x) < 128)]
+    print(len(ls_filter))
+count_ACSII(s)
