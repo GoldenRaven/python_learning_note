1cc6043149d293a40243df090438e5c175f1834a
GoldenRaven <ligy-_-@outlook.com>
2020-02-16 12:13:24 +0800

    feat: add 17, output error, why?

---
+++ nowcoder/huawei/17.py
@@ -0,0 +1,20 @@
+s = input()
+l = list(s)
+# print(l)
+ll = [l.count(x) for x in l]
+# print(ll)
+min_ = min(ll)
+# print(min_)
+while True:
+    try:
+        lll = ll.index(min_)
+        l.pop(lll)
+        ll.pop(lll)
+        # print(lll,ll)
+        # print(l)
+    except:
+        ss = l[0]
+        for i in l[1:]:
+            ss = ss + i
+        print(ss)
+        break
