a19b631c78d71182595faf7e7bf8ae4fb87cbc3d
GoldenRaven <ligy-_-@outlook.com>
2020-02-16 12:21:41 +0800

    fix: add loop to read input for testifing

--- nowcoder/huawei/17.py
+++ nowcoder/huawei/17.py
@@ -1,20 +1,24 @@
-s = input()
-l = list(s)
-# print(l)
-ll = [l.count(x) for x in l]
-# print(ll)
-min_ = min(ll)
-# print(min_)
 while True:
     try:
-        lll = ll.index(min_)
-        l.pop(lll)
-        ll.pop(lll)
-        # print(lll,ll)
+        s = input()
+        l = list(s)
         # print(l)
+        ll = [l.count(x) for x in l]
+        # print(ll)
+        min_ = min(ll)
+        # print(min_)
+        while True:
+            try:
+                lll = ll.index(min_)
+                l.pop(lll)
+                ll.pop(lll)
+                # print(lll,ll)
+                # print(l)
+            except:
+                ss = l[0]
+                for i in l[1:]:
+                    ss = ss + i
+                print(ss)
+                break
     except:
-        ss = l[0]
-        for i in l[1:]:
-            ss = ss + i
-        print(ss)
         break
