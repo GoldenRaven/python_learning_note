c4e185e063bd15a66004cd6890e1ef6b60035f9d
ligy <ligy-_-@outlook.com>
2020-02-19 16:31:50 +0800

    fix: fix string_replace('0', '') to string_replace('x', '')

--- nowcoder/huawei/24.py
+++ nowcoder/huawei/24.py
@@ -1,18 +1,22 @@
-n = int(input())
-# n = 5
-l = [[0 for i in range(n)] for j in range(n)]
-count = 1
-for i in range(1, n+1):
-    l1 = list(range(i))
-    l1.reverse()
-    l2 = list(range(i))
-    # print(l1, l2)
-    l_index = []
-    index_ = [(l1[j], l2[j]) for j in range(i)]
-    # print(len(index_))
-    for j in range(len(index_)):
-        l[index_[j][0]][index_[j][1]] = count
-        count += 1
-# print(l)
-for i in range(n):
-    print(' '.join(map(str, l[i])).replace('0',''))
+while True:
+    try:
+        n = int(input())
+        # n = 5
+        l = [['x' for i in range(n)] for j in range(n)]
+        count = 1
+        for i in range(1, n+1):
+            l1 = list(range(i))
+            l1.reverse()
+            l2 = list(range(i))
+            # print(l1, l2)
+            l_index = []
+            index_ = [(l1[j], l2[j]) for j in range(i)]
+            # print(len(index_))
+            for j in range(len(index_)):
+                l[index_[j][0]][index_[j][1]] = count
+                count += 1
+        # print(l)
+        for i in range(n):
+            print(' '.join(map(str, l[i])).replace('x',''))
+    except:
+        break
